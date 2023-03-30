3d5533e39869d89681885919549e79e52650b420
HAMPEER <129116624+HAMPEER@users.noreply.github.com>
2023-03-30 16:50:08 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,19 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    led.plotBarGraph(
+    receivedNumber,
+    0
+    )
+})
+let mode = 0
+radio.setGroup(255)
+basic.forever(function () {
+    if (0 == 0) {
+    	
+    } else if (0 == 0) {
+    	
+    } else if (0 == 0) {
+    	
+    } else {
+    	
+    }
+})
