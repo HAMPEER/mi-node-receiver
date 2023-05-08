8b83fa9e474737e893521e06ccff767bf4995713
HAMPEER <129116624+HAMPEER@users.noreply.github.com>
2023-05-08 14:12:31 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    led.plotBarGraph(
-    receivedNumber,
-    0
-    )
+    mode = receivedNumber
+    if (minode.switchIsOpened(ConnName.A0)) {
+        led.plotBarGraph(
+        receivedNumber,
+        diagram
+        )
+    }
 })
+let diagram = 0
 let mode = 0
+mode = 0
 radio.setGroup(255)
 basic.forever(function () {
-    if (0 == 0) {
-    	
-    } else if (0 == 0) {
-    	
-    } else if (0 == 0) {
-    	
-    } else {
-    	
+    if (mode == 0) {
+        diagram = 35
+    } else if (mode == 1) {
+        diagram = 5
+    } else if (mode == 2) {
+        diagram = 100
+    } else if (mode == 3) {
+        diagram = 1023
     }
 })
